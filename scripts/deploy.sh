f955e95299e5780c1f1b694b9bf6c3f6e555e466
hyunssii <hyunssii@naver.com>
2024-08-21 23:32:34 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,22 +3,31 @@
 REPOSITORY=/home/ubuntu/sesubway
 FLASK_APP_DIR=/home/ubuntu/sesubway
 ENV_PATH=$FLASK_APP_DIR/.env
+cd $REPOSITORY
 
-# .env 파일 로드 (존재할 경우)
+# Flask 앱 인스턴스 종료
+FLASK_PID=$(pgrep -f gunicorn)
+if [ -z "$FLASK_PID" ]; then
+  echo "> 종료할 Flask 애플리케이션이 없습니다."
+else
+  echo "> Flask 애플리케이션 종료 중, PID: $FLASK_PID"
+  kill -15 $FLASK_PID
+  sleep 5
+fi
+
+# 환경 변수 로드
 if [ -f $ENV_PATH ]; then
     source $ENV_PATH
 fi
 
-# 가상 환경 설정 및 패키지 설치
-echo "> Setting up new virtual environment"
+echo "> 기존 가상 환경 디렉터리 제거"
+rm -rf $FLASK_APP_DIR/venv
+
+echo "> 새로운 가상 환경 설정"
 python3 -m venv $FLASK_APP_DIR/venv
 source $FLASK_APP_DIR/venv/bin/activate
 
-echo "> Installing dependencies"
-pip install -r $FLASK_APP_DIR/requirements.txt
-
-# Flask 애플리케이션 시작
-echo "> Starting Flask app with gunicorn"
+# Flask 앱 시작
+echo "> Flask 앱을 Gunicorn으로 시작"
 cd $FLASK_APP_DIR
-source $FLASK_APP_DIR/venv/bin/activate
 nohup gunicorn -w 4 app:app -b 0.0.0.0:5002 > /dev/null 2> /dev/null < /dev/null &
